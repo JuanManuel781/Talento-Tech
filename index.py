0457ac7b38f8ca89422b19a6066311159dcac4d6
Juan_Manuel <Juanmanuelguaquespud@gmail.com>
2024-06-22 09:53:35 -0500

    Suma de dos numeros

--- index.py
+++ index.py
@@ -5,3 +5,7 @@ mi_tupla = (1, 2, 3, 2, 4, 5)
 
 print('esta es una lista: ',mi_lista)
 print('esta es una tupla:',mi_tupla)
+
+Primer_numero = input("Ingrese un numero: ")
+Segundo_numero = input("Ingrese un numero: ")
+resultado = print('resultado es:', (int(Primer_numero) * int(Segundo_numero)))
